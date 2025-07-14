a30b3beb258eca9574c49e10761e4e7e8f90b4f2
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-07-15 01:41:04 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/login_screen.dart'; // You’ll create this
+import 'screens/login_screen.dart'; // You'll create this
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
