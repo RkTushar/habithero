68294dbbdea43ab32d0f33bbb377f2315c34476c
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-07-23 02:07:23 +0600

    Update habit_detail_screen.dart

--- lib/screens/habit_detail_screen.dart
+++ lib/screens/habit_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HabitDetailScreen extends StatefulWidget {
   final String habitId;
