745549e9ab51e4b964ce11f9316c827d82dd6454
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-07-27 00:47:48 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/login_screen.dart'; // You'll create this
 import 'firebase_options.dart';
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await NotificationService.init();
   runApp(MyApp());
 }
 
